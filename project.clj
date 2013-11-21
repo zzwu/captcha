74661bb122f424173f1b2ab832a6080c2c122ed9
zzwu <greatzzwu@gmail.com>
2013-11-21 15:42:54 +0800

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject captcha "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
-  :dev-dependencies [[midje "1.3-alpha5"]])
+  :dependencies [[org.clojure/clojure "1.5.0"]]
+  :profiles {:dev {:dependencies [[midje "1.5.1"]]
+                   :plugins [[lein-midje "3.1.1"]]}}])

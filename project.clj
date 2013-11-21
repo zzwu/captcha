e3eae17c3460a2bf0451e37ab21852af6d4c7741
zzwu <greatzzwu@gmail.com>
2013-11-21 15:44:09 +0800

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject captcha "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.0"]]
   :profiles {:dev {:dependencies [[midje "1.5.1"]]
-                   :plugins [[lein-midje "3.1.1"]]}}])
+                   :plugins [[lein-midje "3.1.1"]]}})
